f8c59df4e747c23140b01ad4c1fe378ea66057d0
EC2 Default User <kazukipump7@gmail.com>
2022-07-28 20:43:13 +0000

    making_header & footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+     customers_mypage_path(resource)
+  end
+
+  def after_sign_up_path_for(resource)
+     about_path
+  end
+
+  # protectedは呼び出された他のコントローラからも参照することができる
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:name, :first_name_kana, :last_name_kana, :first_name, :last_name, :post_code, :phone_number, :address ])
+  end
 end
+# configure_permitted_parametersメソッドでは、devise_parameter_sanitizer.permitメソッドを使うことで
+# ユーザー登録(sign_up)の際に、ユーザー名等のデータ操作を許可している
--- config/routes.rb
+++ config/routes.rb
@@ -6,20 +6,38 @@ Rails.application.routes.draw do
     registrations: "customer/registrations",
     sessions: 'customer/sessions'
   }
-  
+
   # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
-  
+
   # 顧客用
   # URLは変えたくない
   # ファイル構成だけ指定のパスにしたい
   # scope moduleを使う必要がある
   scope module: :customer do
     root :to => "homes#top"
-    get "homes/about" => "homes#about"
+    get "homes/about" => "homes#about", as: "about"
+    get "customers/mypage" => "customers#show"
+    get "customers/unsubscribe" => "customers#unsubscribe", as: "unsubscribe"
+    patch "customers/withdrawal" => "customers#withdrawal", as: "withdrawal"
+    get "orders/confirm" => "orders#confirm", as: "confirm"
+    get "orders/complete" => "orders#complete", as: "complete"
+
+    resources :items
+    resources :customers, only: [:edit,:update,:destroy]
+    resources :orders, only: [:new,:index,:create,:show]
+    resources :shipping_addresses, only: [:index,:create,:edit,:update,:destroy]
+    resources :cart_items, only: [:index,:create,:update,:destroy] do
+      collection do
+        delete "destroy_all"
+      end
+    end
   end
+
+ # 管理者用
+
 
 end
